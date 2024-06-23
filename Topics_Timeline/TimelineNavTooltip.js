e9fbc508027b58e42c2cc1560735e8e71d35a216
WebProjectsHub <143710009+WebProjectsHub@users.noreply.github.com>
2024-06-23 15:06:48 +0400

    Add files via upload

---
+++ Topics_Timeline/TimelineNavTooltip.js
@@ -0,0 +1,48 @@
+var myFullpage = new fullpage
+(
+'#fullpage',
+	{
+        	navigation:true,
+        	showActiveTooltip: false,
+        	navigationTooltips: 
+			[
+			'Egyptian Art',
+			'Greek Art',
+			'Medieval Art',
+			'Renaissance',
+			'Baroque',
+			'Rococo',
+			'Neoclassicism',
+			'Romanticism',
+			'Academic Art',
+			'Realism',
+			'Impressionism',
+			'Post-Impressionism',
+			'Expressionism',
+			'Pointillism',
+			'Naïve Art',
+			'Tonalism',
+			'Symbolism',
+			'Fauvism',
+			'Art Nouveau',
+			'Abstract Expressionism',
+			'Art Deco',
+			'Cubism',
+			'Futurism',
+			'Modernism',
+			'Magical Realism',
+			'Surrealism',
+			'Bauhaus',
+			'Social Realism',
+			'Classical Realism',
+			'Minimalism',
+			'Pop Art',
+			'Contemporary Art',
+			'Photo Realism',
+			'Light and Space',
+			'Street Art',
+			'Digital Art'
+			],
+
+	}
+);
